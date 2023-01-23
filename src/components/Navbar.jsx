f3f4ccf638129e8632c129d375e492e1510892cc
Souvik Mitra <mitrasouvik123@gmail.com>
2023-01-23 21:29:38 +0530

    Deleted BookList component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,17 @@ const Navbar = () => {
                 <h1>All Books App</h1>
             </div>
             <div>Your Favourites</div>
+
+            <div>
+                <form onSubmit={(e) => e.preventDefault()}>
+                    <input type="text"
+                        placeholder='Search Book...'
+
+
+
+                    />
+                </form>
+            </div>
         </div>
     )
 }
