5785978380098600fb02bec252935aceaa61fa59
Souvik Mitra <mitrasouvik123@gmail.com>
2023-01-23 12:45:18 +0530

    created Navbar Component basic layout

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <div>
+                <h1>All Books App</h1>
+            </div>
+            <div>Your Favourites</div>
+        </div>
+    )
+}
+
+export default Navbar
