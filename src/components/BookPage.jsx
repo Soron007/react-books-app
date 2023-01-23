aa49c4ec0b6d1c25e2b85f9fb05301bd0f30fd53
Souvik Mitra <mitrasouvik123@gmail.com>
2023-01-23 23:27:15 +0530

    imported useParams to get the id from the api url

--- src/components/BookPage.jsx
+++ src/components/BookPage.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { BOOK_API_URL } from '../API';
 
 const BookPage = () => {
+    const [book, setBook] = useState({});
+
+    const { id } = useParams();
+
+    useEffect(() => {
+        axios.get(`${BOOK_API_URL}/${id}`)
+            .then(res => {
+                setBook(res.data)
+            }).catch((err) => {
+                console.log(err);
+            })
+    }, [id])
+
+
     return (
-        <div>
-            BookPage
+        <div className='book-details'>
+
+            <div className='book-image'>
+                <h2>{book?.title}</h2>
+                <img src={book?.image_url} alt="#" />
+            </div>
+
+            <div className='book-description'>
+                <h2>Description</h2>
+                <p>{book?.description}</p>
+                <h2>Authors</h2>
+                <p>{book?.authors}</p>
+                <h2>Genres</h2>
+                <p>{book?.genres}</p>
+
+            </div>
+
         </div>
     )
 }
