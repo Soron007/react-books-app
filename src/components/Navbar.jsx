e1310050706d728dfe72dcf9cdd6a24990685eb0
Souvik Mitra <mitrasouvik123@gmail.com>
2023-01-23 21:30:49 +0530

    added search bar and state

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useAppContext } from '../context/bookContext'
 
 const Navbar = () => {
+
+    const { search, setSearch } = useAppContext();
+
     return (
         <div className='navbar'>
             <div>
@@ -9,13 +13,15 @@ const Navbar = () => {
             <div>Your Favourites</div>
 
             <div>
-                <form onSubmit={(e) => e.preventDefault()}>
+                <form onSubmit={(e) => e.preventDefault()} >
                     <input type="text"
+                        className='input'
                         placeholder='Search Book...'
-
-
-
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
                     />
+
+
                 </form>
             </div>
         </div>
