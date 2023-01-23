58ac8597790493b60189d7b4565f66fe698c315b
Souvik Mitra <mitrasouvik123@gmail.com>
2023-01-23 16:41:30 +0530

    wrapped context around app component

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import BookContextProvider from './context/bookContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 
     <BrowserRouter>
-      <App />
+      <BookContextProvider>
+        <App />
+      </BookContextProvider>
     </BrowserRouter>
 
   </React.StrictMode>
