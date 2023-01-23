c887518c39b8be695edb728ed18118a5ee3a31fa
Souvik Mitra <mitrasouvik123@gmail.com>
2023-01-23 13:00:51 +0530

    added Routes in the App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-
+import { Route, Routes } from 'react-router-dom';
+import BookList from './components/BookList';
+import Favorites from './components/Favorites';
+import BookPage from './components/BookPage';
 function App() {
   return (
     <div className="App">
+
+      <Routes>
+
+        <Route path="/" element={<BookList />} />;
+        <Route path='/book/:id' element={<BookPage />} />
+        <Route path='/favorites' element={<Favorites />} />
+
+
+      </Routes>
 
     </div>
   );
