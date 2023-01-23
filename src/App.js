c9809e87eb106daa43077fd35bed061a78d815a0
Souvik Mitra <mitrasouvik123@gmail.com>
2023-01-23 16:15:05 +0530

    added navbar and footer outside of the routes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 import BookList from './components/BookList';
 import Favorites from './components/Favorites';
 import BookPage from './components/BookPage';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+
 function App() {
   return (
     <div className="App">
-
+      <Navbar />
       <Routes>
 
         <Route path="/" element={<BookList />} />;
@@ -14,6 +18,8 @@ function App() {
 
 
       </Routes>
+
+      <Footer />
 
     </div>
   );
