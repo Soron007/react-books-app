080121291135661425ee6cf0509a96493eba2253
Souvik Mitra <mitrasouvik123@gmail.com>
2023-01-23 22:52:30 +0530

    created favorites page, search func and link

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
 import '../App.css';
+import { useAppContext } from '../context/bookContext';
 
 const Favorites = () => {
+    const { search, favorites, addToFav, removeFromFav } = useAppContext();
+
+    const favChecker = (id) => {
+        const boolean = favorites.some((book) => book.id === id);
+        return boolean;
+    }
+
+
     return (
-        <div>
-            Favourites
+
+        <div className='favorites'>
+
+            {favorites.filter((fav) => {
+
+                if (search === '') {
+                    return fav;
+                } else if (fav.title.toLowerCase().includes(search.toLowerCase())) {
+                    return fav;
+                }
+            }).map((fav) => (
+                <div key={fav.id} className='book'>
+
+                    <div><h4>{fav.title}</h4></div>
+
+                    <div><img src={fav.image_url} alt="#" /></div>
+
+                    <div>
+
+                        {favChecker(fav.id) ? <button onClick={() => removeFromFav(fav.id)}>Remove from favorites</button> :
+                            <button onClick={() => addToFav(fav)}>Add to favorites</button>}
+                    </div>
+
+                </div>
+            ))}
         </div>
     )
 }
